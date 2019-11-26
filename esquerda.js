4c7a6cc1308e0abc100067258eae9a34cffd8a58
ARTHUR PC <arthurpimetelcordeiro@gmail.com>
2019-11-26 12:09:34 -0300

    onibus funcionando na master

--- esquerda.js
+++ esquerda.js
@@ -2,77 +2,73 @@ let valores = []
 
 var clicado1 = false
 
-function janela1(e) {
-   e.preventDefault();
-   
-   var mapa = e.target;
-
-   
-    
-       var coordenadas = mapa.getAttribute("coords");
-       var coords_array = coordenadas.split(",");
-       var coords_clip = '';
-       
-       for(let i in coords_array){
-          if(i%2 == 0) coords_clip += coords_array[i]+"px "+coords_array[+i+1]+"px,"
-       }
-       
-       coords_clip = coords_clip.substr(0, coords_clip.length-1);
-
-        let span = document.createElement("span");
-        span.className = "clip";
-        span.setAttribute("onclick", "this.outerHTML=''");
-        span.style = "clip-path: polygon("+coords_clip+");";
-
-       document.getElementById("container").appendChild(span);
-       
-       if(!clicado1){
+function janela1() {
+        var imagem = document.createElement('img')
+        var resultado = document.querySelector('#res') 
+        imagem.setAttribute('src','Janela1.png')
+        resultado.style.textAlign = 'left'
+        resultado.innerHTML = ``
+    if(!clicado1){
         valores.push('janela 1')
         clicado1 = true
-
+        resultado.appendChild(imagem)
+        
     }else{
         valores.pop()
         clicado1 = false
         
-
     }
+    
+    
+  
 }
 
 var clicado2 = false
-function janela2(e) {
-    e.preventDefault();
+function janela2() {
+     var img2 = document.createElement('img')
+    img2.setAttribute('src','traseira.png')
     
-    var mapa = e.target;
- 
+    res2.style.textAlign = 'left'
+    res2.innerHTML = ``
+
      if(!clicado2){
          valores.push('janela 2')
-         clicado2 = true
+        clicado2 = true
+        res2.appendChild(img2)
+  
+ 
+
      }else{
          valores.pop()
          clicado2 = false
-     }
-     
-        var coordenadas = mapa.getAttribute("coords");
-        var coords_array = coordenadas.split(",");
-        var coords_clip = '';
-        
-        for(let i in coords_array){
-           if(i%2 == 0) coords_clip += coords_array[i]+"px "+coords_array[+i+1]+"px,"
-        }
-        
-        coords_clip = coords_clip.substr(0, coords_clip.length-1);
- 
-         let span = document.createElement("span");
-         span.className = "clip1";
-         span.setAttribute("onclick", "this.outerHTML=''");
-         span.style = "clip-path: polygon("+coords_clip+");";
- 
-        document.getElementById("container").appendChild(span);
-       
- }
 
+
+     }
+
+    
+}
+
+var clicado3 = false
 function janela3(){
+    var img3 = document.createElement('img')
+     img3.setAttribute('src','direita.png') 
+    res3.style.textAlign = 'center'
+    res3.innerHTML = ``
+
+    if(!clicado3){
     valores.push('janela 3')
+    clicado3 = true
+    res3.appendChild(img3)
+    
+    }else{
+    valores.pop()
+    clicado3 = false
+}
+    
+    
+    
+
+
 }
 function janela4(){
     valores.push('janela 4')  
@@ -94,6 +90,7 @@ function lado1(){
 }
 
 function limpar(){
+   
     valores.length = 0
     
     clicado1 = false 
