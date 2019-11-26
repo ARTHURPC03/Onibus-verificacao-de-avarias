d2cbc0137d5e9899ca41119c80af14d4645ce6f7
ARTHUR PC <arthurpimetelcordeiro@gmail.com>
2019-11-25 21:33:52 -0300

    onibus funcionando

--- x.js
+++ x.js
@@ -2,48 +2,28 @@ let valores = []
 
 var clicado1 = false
 
-function janela1(e) {
-   e.preventDefault();
-   
-   var mapa = e.target;
-
-   
-    
-       var coordenadas = mapa.getAttribute("coords");
-       var coords_array = coordenadas.split(",");
-       var coords_clip = '';
-       
-       for(let i in coords_array){
-          if(i%2 == 0) coords_clip += coords_array[i]+"px "+coords_array[+i+1]+"px,"
-       }
-       
-       coords_clip = coords_clip.substr(0, coords_clip.length-1);
-
-        let span = document.createElement("span");
-        span.className = "clip";
-        span.setAttribute("onclick", "this.outerHTML=''");
-        span.style = "clip-path: polygon("+coords_clip+");";
-
-       document.getElementById("container").appendChild(span);
-       
+function janela1() {
        if(!clicado1){
         valores.push('janela 1')
         clicado1 = true
-
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        img.setAttribute('src','frente.png')
+        
     }else{
         valores.pop()
         clicado1 = false
         
-
+        
     }
+    res.style.textAlign = 'center'
+    res.innerHTML = `Detectamos as seguintes avarias:`
+    res.appendChild(img)
 }
 
 var clicado2 = false
-function janela2(e) {
-    e.preventDefault();
-    
-    var mapa = e.target;
- 
+function janela2() {
+
      if(!clicado2){
          valores.push('janela 2')
          clicado2 = true
@@ -51,25 +31,7 @@ function janela2(e) {
          valores.pop()
          clicado2 = false
      }
-     
-        var coordenadas = mapa.getAttribute("coords");
-        var coords_array = coordenadas.split(",");
-        var coords_clip = '';
-        
-        for(let i in coords_array){
-           if(i%2 == 0) coords_clip += coords_array[i]+"px "+coords_array[+i+1]+"px,"
-        }
-        
-        coords_clip = coords_clip.substr(0, coords_clip.length-1);
- 
-         let span = document.createElement("span");
-         span.className = "clip1";
-         span.setAttribute("onclick", "this.outerHTML=''");
-         span.style = "clip-path: polygon("+coords_clip+");";
- 
-        document.getElementById("container").appendChild(span);
-       
- }
+}
 
 function janela3(){
     valores.push('janela 3')
